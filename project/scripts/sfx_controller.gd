c124633aaef808e0a661a205ee7f2c3e869b9989
arko <arkadiusz.kupracz@gmail.com>
2026-09-06 21:00:51 +0100

    add sfx controller

---
+++ project/scripts/sfx_controller.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var _audio_stream_pool_size := 16
+
+var _audio_stream_player_pool: Array[AudioStreamPlayer]
+
+
+func _ready() -> void:
+	for i in _audio_stream_pool_size:
+		var audio_stream_player := AudioStreamPlayer.new()
+		audio_stream_player.bus = "SFX"
+
+		_audio_stream_player_pool.append(audio_stream_player)
+
+
+func play(audio_stream: AudioStream) -> void:
+	for player in _audio_stream_player_pool:
+		if not player.playing:
+			player.stream = audio_stream
+			player.play()
